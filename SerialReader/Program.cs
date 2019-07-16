798acf19a65c69c15bc7c4b7e6f9e4fb8fee9351
TransformerLifeManagement <TransformerLifeManagement@outlook.com>
2019-07-16 13:50:57 +0430

    Add project files.

---
+++ SerialReader/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+
+
+namespace SerialReader
+{
+    class Program
+    {
+        static SerialPort serialPort = new SerialPort
+        {
+            BaudRate = 115200,
+            StopBits = StopBits.None,
+            Parity=Parity.Even,
+            DataBits=8
+        };
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome");
+            Console.WriteLine("Available Ports:");
+            string[] portnames = SerialPort.GetPortNames();
+            foreach (var p in portnames)
+            {
+                Console.WriteLine(p);
+            }
+            Console.WriteLine("Enter the number of the desired port");
+            string num = Console.ReadLine();
+            serialPort.PortName = "COM" + num;
+            serialPort.DataReceived += SerialPort_DataReceived;
+            serialPort.Open();
+
+        }
+
+        private static void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            sp.
+        }
+    }
+}
